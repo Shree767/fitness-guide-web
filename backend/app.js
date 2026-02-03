f86f33289fe2175dac7354e7a4fefef79f00270d
Shree charan H <shreecharanh@Shrees-MacBook-Pro.local>
2026-02-03 15:50:30 +0530

    Fix start script to use app.js

--- backend/app.js
+++ backend/app.js
@@ -4,38 +4,37 @@ import cors from "cors";
 import { sendEmail } from "./utils/sendEmail.js";
 
 const app = express();
-const router = express.Router();
 
-config({ path: "./config.env" });
+config();
 
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
-    methods: ["POST"],
-    credentials: true,
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST"],
   })
 );
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-router.post("/send/mail", async (req, res, next) => {
+app.post("/send/mail", async (req, res) => {
   const { name, email, message } = req.body;
+
   if (!name || !email || !message) {
-    return next(
-      res.status(400).json({
-        success: false,
-        message: "Please provide all details",
-      })
-    );
+    return res.status(400).json({
+      success: false,
+      message: "Please provide all details",
+    });
   }
+
   try {
     await sendEmail({
-      email: "santhoshkumar00088@gmail.com",
+      email: "shree.charan.7676@gmail.com",
       subject: "GYM WEBSITE CONTACT",
       message,
       userEmail: email,
     });
+
     res.status(200).json({
       success: true,
       message: "Message Sent Successfully.",
@@ -43,13 +42,11 @@ router.post("/send/mail", async (req, res, next) => {
   } catch (error) {
     res.status(500).json({
       success: false,
-      message: " Internal Server Error",
+      message: "Internal Server Error",
     });
   }
 });
 
-app.use(router);
-
 app.listen(process.env.PORT, () => {
-  console.log(`Server listening at port ${process.env.PORT}`);
+  console.log(`Server listening on port ${process.env.PORT}`);
 });
